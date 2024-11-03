76ab3eacb5bc090b7b8ce5e12a764b0ed5fe301a
Thomas Edward Gladwin <thomas.gladwin@gmail.com>
2024-11-03 20:07:42 +0000

    Update Examples.py

--- Examples.py
+++ Examples.py
@@ -31,8 +31,8 @@ environment = Environment.Environment(A_effect_vec, observable_features, MapStri
 environment.set_rewards(pit_punishment=pit_punishment, backtrack_punishment=backtrack_punishment, off_grid_punishment=off_grid_punishment, terminal_reward=terminal_reward)
 
 agent = teg_actorCritic.Agent(environment.nFeatures, environment.nA)
-agent.critic.lambda0 = 0.5
-agent.actor.lambda0 = 0.5
+agent.critic.set_lambda(0.5)
+agent.actor.set_lambda(0.5)
 
 max_episode_length = 1e4
 sim = teg_actorCritic.Simulation(max_episode_length)
@@ -59,8 +59,8 @@ environment = Environment.Environment(A_effect_vec, observable_features, nR=nR, 
 environment.set_rewards(pit_punishment=pit_punishment, backtrack_punishment=backtrack_punishment, off_grid_punishment=off_grid_punishment, terminal_reward=terminal_reward)
 
 agent = teg_actorCritic.Agent(environment.nFeatures, environment.nA)
-agent.critic.lambda0 = 0
-agent.actor.lambda0 = 0
+agent.critic.set_lambda(0)
+agent.actor.set_lambda(0)
 
 max_episode_length = 1e6
 sim = teg_actorCritic.Simulation(max_episode_length)
@@ -89,15 +89,15 @@ off_grid_punishment = -1
 terminal_reward = 1
 observable_features = (True, False, False)
 nEpisodes = 1e2
-lambda_critic = 0.0
-lambda_actor = 0.0
+lambda_critic = 0.5
+lambda_actor = 0.5
 
 environment = Environment.Environment(A_effect_vec, observable_features, MapString=MapString, random_pit_prob=random_pit_prob)
 environment.set_rewards(pit_punishment=pit_punishment, backtrack_punishment=backtrack_punishment, off_grid_punishment=off_grid_punishment, terminal_reward=terminal_reward)
 
 agent = teg_actorCritic.Agent(environment.nFeatures, environment.nA)
-agent.critic.lambda0 = lambda_critic
-agent.actor.lambda0 = lambda_actor
+agent.critic.set_lambda(lambda_critic)
+agent.actor.set_lambda(lambda_actor)
 
 max_episode_length = 1e6
 sim = teg_actorCritic.Simulation(max_episode_length)
