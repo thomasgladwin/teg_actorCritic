10d6ad60bad0f30445ce28c6b15e04119902a81a
Thomas Edward Gladwin <thomas.gladwin@gmail.com>
2025-01-01 19:24:21 +0000

    Update Examples.py

--- Examples.py
+++ Examples.py
@@ -76,33 +76,45 @@ sim.plots(environment, agent, routes)
 # MapString defines the world: 0 = default state, 1 = start, 2 = terminal, 3 = pit, 4 = wall.
 
 # A_effect_vec = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 0], [0, 1], [1, -1], [1, 0], [1, 1]]
-A_effect_vec = [[-1, 0], [0, -1], [0, 1], [1, 0]]
 MapString = '''
 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4
-4 2 3 3 3 3 3 1 0 0 0 0 0 0 4
+4 2 3 3 3 3 3 3 1 0 0 0 0 0 4
 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4
 '''
-random_pit_prob = 0.0
-pit_punishment = -1
-backtrack_punishment = 0
-off_grid_punishment = -1
-terminal_reward = 1
-observable_features = (True, False, False)
-nEpisodes = 1e2
-lambda_critic = 0.0
-lambda_actor = 0.0
+nItsPerCoeffComb = 30
+routes_per_c = []
+for lambda_it_c in [0.01, 0.99]:
+    for lambda_it_a in [lambda_it_c]:
+        ep_lens = []
+        for iIt in range(nItsPerCoeffComb):
+            A_effect_vec = [[-1, 0], [0, -1], [0, 1], [1, 0]]
+            random_pit_prob = 0.0
+            pit_punishment = -1
+            backtrack_punishment = 0
+            off_grid_punishment = -1
+            terminal_reward = 1
+            observable_features = (True, False, False)
+            nEpisodes = 5e2
+            max_episode_length = 1e4
+            lambda_critic = lambda_it_c
+            lambda_actor = lambda_it_a
 
-environment = Environment.Environment(A_effect_vec, observable_features, MapString=MapString, random_pit_prob=random_pit_prob)
-environment.set_rewards(pit_punishment=pit_punishment, backtrack_punishment=backtrack_punishment, off_grid_punishment=off_grid_punishment, terminal_reward=terminal_reward)
+            environment = Environment.Environment(A_effect_vec, observable_features, MapString=MapString, random_pit_prob=random_pit_prob)
+            environment.set_rewards(pit_punishment=pit_punishment, backtrack_punishment=backtrack_punishment, off_grid_punishment=off_grid_punishment, terminal_reward=terminal_reward)
 
-agent = teg_actorCritic.Agent(environment.nFeatures, environment.nA)
-agent.critic.set_lambda(lambda_critic)
-agent.actor.set_lambda(lambda_actor)
+            agent = teg_actorCritic.Agent(environment.nFeatures, environment.nA)
+            agent.critic.set_lambda(lambda_critic)
+            agent.actor.set_lambda(lambda_actor)
 
-max_episode_length = 1e6
-sim = teg_actorCritic.Simulation(max_episode_length)
+            sim = teg_actorCritic.Simulation(max_episode_length)
 
-agent = sim.train(nEpisodes, environment, agent)
-routes = sim.test(environment, agent, 5)
-sim.plots(environment, agent, routes)
+            agent = sim.train(nEpisodes, environment, agent)
+            routes = sim.test(environment, agent, 5)
+            #sim.plots(environment, agent, routes)
 
+            routes_per_c.append(routes.copy())
+            ep_lens.append(sim.ep_len.copy())
+
+        ep_len_smoothed = np.array(ep_lens).mean(axis=0)
+        sim.ep_len = ep_len_smoothed
+        sim.plots(environment, agent, routes)
